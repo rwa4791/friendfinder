040d568d6c7d59d960d8ee710317f4cb0367110e
Bobby Angus <Bobby Angus>
2017-11-26 22:02:08 -0500

    changes

---
+++ app/data/friend.js
@@ -0,0 +1,19 @@
+var friendList = [
+  {
+    name: 'John McGrory',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  }
+]
+
+module.exports = friendList;
